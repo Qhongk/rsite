129f9a482f0b1274abce7ea8d338a43530daa7ad
hon_shop <kqhon@foxmail.com>
2015-09-18 17:43:34 +0800

    mongo

---
+++ rsite-api/src/main/java/com/kza/rsite/service/MongoService.java
@@ -0,0 +1,52 @@
+package com.kza.rsite.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+/**
+ * Created by kza on 2015/9/18.
+ */
+public class MongoService {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public void queryUser() {
+        List<Person> users = mongoTemplate.findAll(Person.class, "User");
+        for (Person user : users) {
+            System.out.println(user + "***************************************");
+        }
+    }
+
+    @Document
+    public class Person {
+
+        private String id;
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getId() {
+            return id;
+        }
+        public String getName() {
+            return name;
+        }
+        public int getAge() {
+            return age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person [id=" + id + ", name=" + name + ", age=" + age + "]";
+        }
+
+    }
+}
