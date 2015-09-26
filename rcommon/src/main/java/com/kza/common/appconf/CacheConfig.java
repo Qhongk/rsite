6f1f8ffa473cd3744a0d919cf94ba99ee324b248
hon <kqhon@foxmail.com>
2015-09-26 19:44:50 +0800

    cache manager bean

--- rcommon/src/main/java/com/kza/common/appconf/CacheConfig.java
+++ rcommon/src/main/java/com/kza/common/appconf/CacheConfig.java
@@ -1,8 +1,18 @@
 package com.kza.common.appconf;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.CacheResolver;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import java.util.Arrays;
 
 /**
  * Created by zaza on 2015/9/26.
@@ -10,5 +20,28 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import(value = {RedisConfig.class})
 @EnableCaching
-public class CacheConfig {
+public class CacheConfig implements CachingConfigurer {
+
+    @Bean
+    @Override
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(Arrays.asList(new ConcurrentMapCache("default")));
+        return cacheManager;
+    }
+
+    @Override
+    public CacheResolver cacheResolver() {
+        return null;
+    }
+
+    @Override
+    public KeyGenerator keyGenerator() {
+        return null;
+    }
+
+    @Override
+    public CacheErrorHandler errorHandler() {
+        return null;
+    }
 }
--- rsite-api/src/main/java/com/kza/rsite/appconf/WebApplicationConfig.java
+++ rsite-api/src/main/java/com/kza/rsite/appconf/WebApplicationConfig.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Controller;
  * Created by kza on 2015/9/24.
  */
 @Configuration
-@ComponentScan(basePackages = "com.kza.*", excludeFilters = {@ComponentScan.Filter(value = {Controller.class})})
+@ComponentScan(basePackages = "com.kza.*", excludeFilters = {
+        @ComponentScan.Filter(value = {Controller.class}),
+        @ComponentScan.Filter(value = {Configuration.class})
+})
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @Import(value = {CacheConfig.class, MongoConfig.class})
 @PropertySource(value = "classpath:globals.properties", name = "properties")
