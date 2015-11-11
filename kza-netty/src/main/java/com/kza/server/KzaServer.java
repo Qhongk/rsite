a0cbfeef3fdd348029a6e33c69dada3ecf17195e
hon_shop <kqhon@foxmail.com>
2015-11-11 19:48:24 +0800

    update server

--- kza-netty/src/main/java/com/kza/server/KzaServer.java
+++ kza-netty/src/main/java/com/kza/server/KzaServer.java
@@ -2,10 +2,14 @@ package com.kza.server;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -35,9 +39,20 @@ public class KzaServer {
         }
     });
 
-    public void start(final int port, final ChannelInitializer<? extends Channel> pipelineFactory) {
+    public int start(final int port, final ChannelInitializer<? extends Channel> pipelineFactory) throws InterruptedException {
 
         ServerBootstrap bootstrap = new ServerBootstrap();
-        bootstrap.group(bossGroup, workerGroup);
+        bootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(pipelineFactory);
+
+        ChannelFuture future = bootstrap.bind(port).sync();
+        try {
+            future = bootstrap.bind(port).sync();
+            SocketAddress socketAddress = future.channel().localAddress();
+            return ((InetSocketAddress) socketAddress).getPort();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
